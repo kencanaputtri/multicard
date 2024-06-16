cf3b757fd40911efb247bff525ebc8c9bf2112e4
kencanaputtri <147846034+kencanaputtri@users.noreply.github.com>
2024-06-16 10:01:34 +0700

    Update database.php

--- service/database.php
+++ service/database.php
@@ -4,3 +4,24 @@ $username = "root";
 $password = "";
 $database_name = "multicard";
 $db = mysqli_connect($hostname, $username, $password, $database_name);
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Mengecek koneksi
+if ($db->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Mendapatkan data dari NodeMCU
+$card_id = $_POST["card_id"];
+// Menyimpan data ke database
+$sql = "INSERT INTO card (card_ID) VALUES ('".$card_id."')";
+
+if ($db->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $db->error;
+}
+
+$db->close();
+?>
