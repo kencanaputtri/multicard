715e540daa2f5b30db5e3ffe410fdc8deeffbcc6
kencanaputtri <147846034+kencanaputtri@users.noreply.github.com>
2024-06-16 12:44:05 +0700

    a

--- login.php
+++ login.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<F, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+<body>
+    <?php include "layout/header.html"?>
+        <h3>MASUK dengan google</h3>
+        <i><?= $login_message ?></i>
+        <form action="login.php" method="POST">
+            <input type="text" placeholder="username" name="username"/>
+            <input type="password" placeholder="password" name="password"/>
+            <button type = "submit" name="login">MASUK SEKARANG</button>
+        </form>
+    <?php include "layout/footer.html"?>
+</body>
+</html>
+
 <?php
     include "service/database.php";
     session_start();
@@ -30,22 +50,3 @@
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=<F, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="login.css">
-<body>
-    <?php include "layout/header.html"?>
-        <h3>MASUK dengan google</h3>
-        <i><?= $login_message ?></i>
-        <form action="login.php" method="POST">
-            <input type="text" placeholder="username" name="username"/>
-            <input type="password" placeholder="password" name="password"/>
-            <button type = "submit" name="login">MASUK SEKARANG</button>
-        </form>
-    <?php include "layout/footer.html"?>
-</body>
-</html>
